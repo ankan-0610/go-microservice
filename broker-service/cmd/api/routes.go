4b4654195a993db0427d362b2c5052051f89b97b
Ankan Dey <86412494+ankan-0610@users.noreply.github.com>
2024-09-16 15:18:59 +0530

    .

--- broker-service/cmd/api/routes.go
+++ broker-service/cmd/api/routes.go
@@ -3,10 +3,11 @@ package main
 import(
 	"net/http"
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/go-chi/cors"
 )
 
-func routes() http.Handler{
+func (app *Config) routes() http.Handler{
 	mux := chi.NewRouter
 
 	mux.Use(cors.Handler(cors.Options{
@@ -17,4 +18,10 @@ func routes() http.Handler{
 		AllowCredentials: true,
 		MaxAge: 300,
 	}))
+
+	mux.Use(middleware.Heartbeat("/ping"))
+
+	mux.Post("/", app.Broker)
+
+	return mux
 }
